b894d4a907a5c2625def9958500d5a19a276ec65
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2022-11-22 22:24:21 +0600

    sfcs
    fsds

---
+++ Flutter/Source Code/15 Tab Bar  Tab View Fragment/Fragment/BalanceFragment.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+class BlanceFragment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("BlanceFragment"),
+      ),
+    );
+  }
+}
---
+++ Flutter/Source Code/15 Tab Bar  Tab View Fragment/Fragment/EmailFragment.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+class EmailFragment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("EmailFragment"),
+      ),
+    );
+  }
+}
