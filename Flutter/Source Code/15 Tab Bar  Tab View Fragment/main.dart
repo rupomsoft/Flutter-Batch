ceab0038bb1bfd95d4a4dccb6ef17493f0e5e4f7
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2022-11-22 22:20:53 +0600

    sdfsd
    fsfsdf

--- Flutter/Source Code/15 Tab Bar  Tab View Fragment/main.dart
+++ Flutter/Source Code/15 Tab Bar  Tab View Fragment/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'Fragment/BalanceFragment.dart';
-import 'Fragment/ContactFragment.dart';
-import 'Fragment/EmailFragment.dart';
-import 'Fragment/HomeFragment.dart';
-import 'Fragment/PersonFragment.dart';
-import 'Fragment/SearchFragment.dart';
-import 'Fragment/SettingsFragment.dart';
-import 'Fragment/AlarmFragment.dart';
-
 
 main(){
   runApp(const MyApp()); // Application
@@ -29,38 +20,8 @@ class HomeActivity extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return DefaultTabController(
-        length: 8,
-        child: Scaffold(
-          appBar: AppBar(
-            title:Text( "My App"),
-            bottom: TabBar(
-              isScrollable:true,
-              tabs: [
-                Tab(icon: Icon(Icons.home),text: 'Home',),
-                Tab(icon: Icon(Icons.search),text: 'search',),
-                Tab(icon: Icon(Icons.settings),text: 'settings',),
-                Tab(icon: Icon(Icons.email),text: 'email',),
-                Tab(icon: Icon(Icons.contact_mail),text: 'contact',),
-                Tab(icon: Icon(Icons.person),text: 'person',),
-                Tab(icon: Icon(Icons.access_alarm),text: 'alarm',),
-                Tab(icon: Icon(Icons.account_balance),text: 'balance',)
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-                HomeFragment(),
-                SearchFragment(),
-                SettingsFragment(),
-                EmailFragment(),
-                ContactFragment(),
-                PersonFragment(),
-                AlarmFragment(),
-                BlanceFragment()
-            ],
-          ),
-        )
+    return Scaffold(
+      appBar: AppBar(title: Text('Hello')),
     );
 
   }
