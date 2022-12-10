69478e4c5338a6f78bc3e426964a884943085437
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2022-12-10 23:32:03 +0600

    Sum  Updated

--- Flutter/Source Code/23 Stateful Widget/Simple Calculator/lib/Style.dart
+++ Flutter/Source Code/23 Stateful Widget/Simple Calculator/lib/Style.dart
@@ -1,38 +1,29 @@
-
 import 'package:flutter/material.dart';
 
 InputDecoration AppInputStyle(label){
   return InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
-      fillColor: Colors.black12,
-      border:OutlineInputBorder(),
-      labelText: label
+    contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+    filled: true,
+    fillColor: Colors.white,
+    border: OutlineInputBorder(),
+    labelText: label
   );
 }
 
-TextStyle HeaTextStyle(){
+TextStyle HeadTextStyle(){
   return TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.w400
-  );
-}
-
-SizedBox SizedBox100(child){
-  return SizedBox(
-    height: 100,
-    width: double.infinity,
-    child:child,
+    fontSize: 28,
+    fontWeight: FontWeight.w800
   );
 }
 
 
 ButtonStyle AppButtonStyle(){
   return ElevatedButton.styleFrom(
-      padding: EdgeInsets.all(20),
-      backgroundColor: Colors.green,
-      foregroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(4))
-      )
+    padding: EdgeInsets.all(22),
+    backgroundColor: Colors.green,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4))
+    )
   );
 }
--- Flutter/Source Code/23 Stateful Widget/Simple Calculator/lib/main.dart
+++ Flutter/Source Code/23 Stateful Widget/Simple Calculator/lib/main.dart
@@ -1,91 +1,109 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'Style.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  MyHomePage(),
+      title: "Sum App",
+      home: MyHomePage(),
     );
   }
 }
 
-
-
-
-class MyHomePage extends StatefulWidget{
-  double num1=0;
-  double num2=0;
-  double sum=0;
-
+class MyHomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyHomePageUi();
+    return MyHomePageUI();
   }
 }
 
-class MyHomePageUi extends State<MyHomePage>{
+class MyHomePageUI extends State<MyHomePage> {
 
-
-
-  ButtonOnClick(){
-   double res=widget.num1+widget.num2;
-   setState(() {
-     widget.sum=res;
-   });
-  }
-
-  InputOnChange(num,content){
-    setState(() {
-      switch (num) {
-        case "num1":
-          widget.num1=double.parse(content);
-          break;
-        case "num2":
-          widget.num2=double.parse(content);
-          break;
-      }
-    });
-  }
+  Map<String,double> FormValue={"Num1":0, "Num2":0, "Num3":0};
+  double SUM=0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Sum App')),
-      body: Container(
-        padding: EdgeInsets.all(35),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 10),
-              Text("Sum is=${widget.sum}",style: HeaTextStyle()),
-              SizedBox(height: 10),
-              TextField(keyboardType: TextInputType.number,decoration: AppInputStyle("First Number"), onChanged: (content){InputOnChange("num1",content);}),
-              SizedBox(height: 20),
-              TextField(keyboardType: TextInputType.number,decoration: AppInputStyle("Second Number"),onChanged: (content){InputOnChange('num2',content);}),
-              SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                child:ElevatedButton(style:AppButtonStyle(),child: Text('Add'),onPressed: (){ ButtonOnClick();}),
-              ),
-            ],
-          ),
-        )
-      )
 
+
+    MyInputOnChange(InputKey,InputValue){
+      setState(() {
+        FormValue.update(InputKey, (value) => double.parse(InputValue));
+      });
+    }
+
+
+    AddAllNumber(){
+      setState((){
+        SUM=FormValue['Num1']!+FormValue['Num2']!+FormValue['Num3']!;
+      });
+    }
+
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Add"),),
+      body: Padding(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            Text(SUM.toString(),style: HeadTextStyle(),),
+
+
+            SizedBox(height: 20,),
+
+            TextFormField(onChanged: (value){
+              MyInputOnChange("Num1",value);
+            }, decoration: AppInputStyle("First Number"),),
+
+
+            SizedBox(height: 20,),
+
+
+            TextFormField(onChanged: (value){
+              MyInputOnChange("Num2",value);
+            }, decoration: AppInputStyle("Second Number"),),
+
+
+            SizedBox(height: 20,),
+
+
+            TextFormField(onChanged: (value){
+              MyInputOnChange("Num3",value);
+            }, decoration: AppInputStyle("Third Number"),),
+
+
+
+            SizedBox(height: 20,),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+              style: AppButtonStyle(),
+              child: Text('Add'),
+              onPressed: (){
+                AddAllNumber();
+              },
+            ),)
+
+
+          ],
+        ),
+      )
     );
 
   }
 
+
+
+
 }
+
